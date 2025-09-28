199851faee97cb68ce73ccb73d06e4a0643df44e
Анастасия Фёдорова <anastasiyafedorova2266@gmail.com>
2025-09-29 00:36:56 +0700

    Add CatEntity

---
+++ CatEntity/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatEntity
+{
+    public interface IDomainObject
+    {
+        int Id { get; set; }
+    }
+    public class Cat : IDomainObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public int Age { get; set; }
+    }
+}
