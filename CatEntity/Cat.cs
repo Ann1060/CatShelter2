f560dd696970ecd354ea3fb995e44283265e29b2
Anastasiya <anastasiya10602266@gmail.com>
2025-11-26 23:56:53 +0700

    Переделан класс Cat с использованием INotifyPropertyChanged

--- CatEntity/Cat.cs
+++ CatEntity/Cat.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace CatEntity
 {
@@ -10,11 +12,60 @@ namespace CatEntity
     {
         int Id { get; set; }
     }
-    public class Cat : IDomainObject
+
+    public class Cat : IDomainObject, INotifyPropertyChanged
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Breed { get; set; }
-        public int Age { get; set; }
+        private int id;
+        private string name;
+        private string breed;
+        private int age;
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string Breed
+        {
+            get { return breed; }
+            set
+            {
+                breed = value;
+                OnPropertyChanged("Breed");
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
